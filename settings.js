ff3a07e9ed8cca0c6cf60f1bc0f9de6e3857ff20
yasirkhaan7 <149736022+yasirkhaan7@users.noreply.github.com>
2025-02-05 17:28:13 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Yas Bot",
+  botOwner: 'Yas Bot', // Your name
+  ownerNumber: '923446236051', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
